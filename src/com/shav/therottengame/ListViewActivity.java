bd090d1d25ba4f535b695c4f83a3465e6ce10807
Harshal Jethwa <hjjethwa@yelp.com>
2014-10-04 18:00:41 -0700

    updated listview on single player

--- src/com/shav/therottengame/ListViewActivity.java
+++ src/com/shav/therottengame/ListViewActivity.java
@@ -1,8 +1,7 @@
 package com.shav.therottengame;
 
 import java.util.ArrayList;
-
-import com.shav.therottengame.R;
+import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -12,38 +11,39 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class ListViewActivity extends Activity {
+	
+	private ListViewAdapter mAdapter;
+	private ListView mListView;
+	private List<String> mCurrentList;
+	private String mStartingActor;
+	private String mEndingActor;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-
-		final ListView listview = (ListView) findViewById(R.id.listview);
+		mListView = (ListView) findViewById(R.id.listview);
 		String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
 				"Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
 				"Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
 				"OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
 				"Android", "iPhone", "WindowsMobile" };
 
-		final ArrayList<String> list = new ArrayList<String>();
+		mCurrentList = new ArrayList<String>();
 		for (int i = 0; i < values.length; ++i) {
-			list.add(values[i]);
+			mCurrentList.add(values[i]);
 		}
-		final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, list);
-		listview.setAdapter(adapter);
-
-		listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-			public void onItemClick(AdapterView<?> parent, final View view,
-					int position, long id) {
-				final String item = (String) parent.getItemAtPosition(position);
-				list.remove(item);
-				adapter.notifyDataSetChanged();
-
-			}
-
-		});
+		
+		mAdapter = new ListViewAdapter(this, mCurrentList);
+		mListView.setAdapter(mAdapter);
+//		mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
+//			public void onItemClick(AdapterView<?> parent, final View view,
+//					int position, long id) {
+//
+//			}
+//		});
 	}
 
 }
