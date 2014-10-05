4c3fdaf2f1d4ac39593b61a0cffd18600c392843
vnamasiv <vnamasiv@uwaterloo.ca>
2014-10-05 11:30:13 -0700

    Update Actor list

--- src/com/shav/therottengame/activity/GameOverActivity.java
+++ src/com/shav/therottengame/activity/GameOverActivity.java
@@ -3,13 +3,15 @@ package com.shav.therottengame.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.games.multiplayer.realtime.Room;
 import com.shav.therottengame.R;
 
-public class GameOverActivity extends Activity {
+public class GameOverActivity extends Activity implements OnClickListener {
 	private TextView mStateTextView;
 	private GoogleApiClient mGoogleApiClient;
 	
@@ -18,7 +20,7 @@ public class GameOverActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game_over);
 		mStateTextView = (TextView) findViewById(R.id.textview_status);
-
+		mStateTextView.setOnClickListener(this);
 		Intent intent = getIntent();
 		boolean wonGame = intent.getBooleanExtra("Won", false);
 		Room room = intent.getParcelableExtra("Room");
@@ -29,5 +31,11 @@ public class GameOverActivity extends Activity {
 			mStateTextView.setText("YOU LOST");
 		}
 
+	}
+
+	@Override
+	public void onClick(View v) {
+		Intent intent = new Intent (this, MainActivity.class);
+		startActivity(intent);
 	}
 }
